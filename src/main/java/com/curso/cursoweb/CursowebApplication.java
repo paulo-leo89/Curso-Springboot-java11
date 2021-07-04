ddc2d89e4b5c02fa4d7c980b6f3a598c05add1ec
paulo-leo89 <pauloads18@gmail.com>
2021-07-04 18:16:17 -0300

    Project created

---
+++ src/main/java/com/curso/cursoweb/CursowebApplication.java
@@ -0,0 +1,13 @@
+package com.curso.cursoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursowebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursowebApplication.class, args);
+	}
+
+}
